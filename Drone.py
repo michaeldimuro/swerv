0b11c2e6fb3bb0da5f6dc346d5cadc8c1aa0efb3
Michael N. Di Muro <michaeldimuro@Michaels-MBP-2.home>
2017-04-11 00:33:20 -0400

    Changed some things in Drone.py

--- Drone.py
+++ Drone.py
@@ -2,6 +2,7 @@
 
 from dji_sdk.dji_drone import DJIDrone
 import dji_sdk.msg
+#import somereel API
 import time
 import sys
 import math
@@ -10,8 +11,15 @@ class SwervDrone:
 
 		def __init__(self):
 			self.drone = DJIDrone()
-			self.reel = SwervReel()
+			#self.reel = SwervReel()
 			self.drone.request_sdk_permission_control()
+            self.drone.arm()
+
+        def obtainControl(self):
+            self.drone.request_sdk_permission_control()
+
+        def releaseControl(self):
+            self.drone.release_sdk_permission_control()
 
         def arm(self):
         	self.drone.arm_drone()
